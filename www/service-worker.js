5a2bc2c3adad29d993fb2bb0a2cc5f9d138a6e71
Pamela Nitschke <pamela.nitschke75@gmail.com>
2026-08-19 22:02:59 +0200

    Aktualisieren von service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,1 +1,39 @@
-.
+const CACHE_NAME = "sol-holo-v1";
+
+const APP_FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
